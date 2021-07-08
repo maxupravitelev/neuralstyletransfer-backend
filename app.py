199fa7f80f16e09eaa489c18031eec13e08e9181
Max Upravitelev <max@upravitelev.de>
2021-07-08 12:25:32 +0200

    add tf init

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 #########################################################################################
+### Built upon: https://www.tensorflow.org/hub/tutorials/tf2_arbitrary_image_stylization
+#########################################################################################
+
+
+#########################################################################################
 ### handle imports
 import time
 #import json
 import sys
 
+# flask imports
 from flask import Response
 from flask import Flask, request
 #from flask_cors import CORS
 from flask import jsonify
+
+
+# tensorflow imports
+import functools
+import os
+
+from matplotlib import gridspec
+import matplotlib.pylab as plt
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
 
 #########################################################################################
 
@@ -16,6 +33,39 @@ from flask import jsonify
 ### init flask
 app = Flask(__name__)
 #CORS(app)
+
+#########################################################################################
+
+#########################################################################################
+### init tensorflow & helper functions
+print("TF Version: ", tf.__version__)
+print("TF-Hub version: ", hub.__version__)
+print("Eager mode enabled: ", tf.executing_eagerly())
+print("GPU available: ", tf.config.list_physical_devices('GPU'))
+
+def crop_center(image):
+  """Returns a cropped square image."""
+  shape = image.shape
+  new_shape = min(shape[1], shape[2])
+  offset_y = max(shape[1] - shape[2], 0) // 2
+  offset_x = max(shape[2] - shape[1], 0) // 2
+  image = tf.image.crop_to_bounding_box(
+      image, offset_y, offset_x, new_shape, new_shape)
+  return image
+
+@functools.lru_cache(maxsize=None)
+def load_image(image_url, image_size=(256, 256), preserve_aspect_ratio=True):
+  """Loads and preprocesses images."""
+  # Cache image file locally.
+  #image_path = tf.keras.utils.get_file(os.path.basename(image_url)[-128:], image_url)
+  image_path = image_url
+  # Load and convert to float32 numpy array, add batch dimension, and normalize to range [0, 1].
+  img = tf.io.decode_image(
+      tf.io.read_file(image_path),
+      channels=3, dtype=tf.float32)[tf.newaxis, ...]
+  img = crop_center(img)
+  img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
+  return img
 
 #########################################################################################
 

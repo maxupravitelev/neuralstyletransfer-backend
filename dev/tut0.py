eb1690c43dcb9614c4fda827aaa29d104e359fc9
Max Upravitelev <max@upravitelev.de>
2021-07-08 16:58:45 +0200

    for loop over filter folder

--- dev/tut0.py
+++ dev/tut0.py
@@ -38,7 +38,7 @@ def img_scaler(image, max_dim = 512):
   scale_ratio = max_dim / max(original_shape)
 
   # Casts a tensor to a new type.
-  new_shape = tf.cast(original_shape * scale_ratio, tf.int32)
+  new_shape = tf.cast(original_shape * scale_ratio * 1, tf.int32)
 
   # Resizes the image based on the scaling constant generated above
   return tf.image.resize(image, new_shape)
@@ -77,39 +77,39 @@ def show_n(images, titles=('',)):
 
 
 
-content_image_url = 'images/l0.jpg' # @param {type:"string"}
-style_image_url = 'images/e0.jpg'  # @param {type:"string"}
-output_image_size = 384  # @param {type:"integer"}
-
-# The content image size can be arbitrary.
-content_img_size = (output_image_size, output_image_size)
-# The style prediction model was trained with image size 256 and it's the 
-# recommended image size for the style image (though, other sizes work as 
-# well but will lead to different results).
-style_img_size = (256, 256)  # Recommended to keep it at 256.
-
-content_image = load_image(content_image_url, content_img_size)
-style_image = load_image(style_image_url, style_img_size)
-style_image = tf.nn.avg_pool(style_image, ksize=[3,3], strides=[1,1], padding='SAME')
-#show_n([content_image, style_image], ['Content image', 'Style image'])
-
 # Load TF-Hub module.
 
-hub_handle = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
+#hub_handle = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
+hub_handle = 'models/arbv1'
 hub_module = hub.load(hub_handle)
 
-outputs = hub_module(content_image, style_image)
-stylized_image = outputs[0]
 
-# Stylize content image with given style image.
-# This is pretty fast within a few milliseconds on a GPU.
+for i in range(47):
 
-outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
-stylized_image = outputs
 
-#stylized_image = outputs[0]
-squeezed_image = tf.squeeze(stylized_image)
-tf.keras.preprocessing.image.save_img("test.jpg", squeezed_image)
+  content_image_url = 'images/k0.jpg' # @param {type:"string"}
+  style_image_url = f"images/f/{i}.jpg"  # @param {type:"string"}
+  output_image_size = 384  # @param {type:"integer"}
+  #output_image_size = 1024  # @param {type:"integer"}
 
-# Visualize input images and the generated stylized image.
-#show_n([content_image, style_image, stylized_image], titles=['Original content image', 'Style image', 'Stylized image'])
+
+  # The content image size can be arbitrary.
+  content_img_size = (output_image_size, output_image_size)
+  # The style prediction model was trained with image size 256 and it's the 
+  # recommended image size for the style image (though, other sizes work as 
+  # well but will lead to different results).
+  style_img_size = (256, 256)  # Recommended to keep it at 256.
+
+  content_image = load_image(content_image_url, content_img_size)
+  style_image = load_image(style_image_url, style_img_size)
+  style_image = tf.nn.avg_pool(style_image, ksize=[3,3], strides=[1,1], padding='SAME')
+
+  #outputs = hub_module(content_image, style_image)
+  #stylized_image = outputs[0]
+
+  outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
+  #stylized_image = outputs
+
+  stylized_image = outputs[0]
+  squeezed_image = tf.squeeze(stylized_image)
+  tf.keras.preprocessing.image.save_img(f"output/k/{i}.jpg", squeezed_image)

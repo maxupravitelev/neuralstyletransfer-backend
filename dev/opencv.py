551916dbea8c12847278381646bb1be41a92469b
Max Upravitelev <max@upravitelev.de>
2021-07-09 09:59:42 +0200

    add setting resolution and fps

--- dev/opencv.py
+++ dev/opencv.py
@@ -7,9 +7,14 @@ frame_height = 768
 
 cap = cv2.VideoCapture(0)
 
+cap.set(3, 640)
+cap.set(4, 480)
+
 time.sleep(1)
+fps = 1 / 5
 
 while (cap.isOpened()):
+    time.sleep(fps)
     _, frame = cap.read()
     cv2.imshow('frame', frame)
 

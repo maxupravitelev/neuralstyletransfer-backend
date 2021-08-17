9d992d89ee3949b61e3f04358aa5436a27dc31b5
Max Upravitelev <max@upravitelev.de>
2021-08-17 16:14:45 +0200

    return filename json after creatiting output file for requesting it via generated_output get request

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ import matplotlib.pylab as plt
 import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
-os.environ["CUDA_VISIBLE_DEVICES"]="-1"    # disable gpu if necessary
+os.environ['CUDA_VISIBLE_DEVICES']='-1'    # disable gpu if necessary
 
 
 # import modules for creating image file strings for storage
@@ -40,14 +40,14 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 #########################################################################################
 ### init tensorflow & helper functions
-print("TF Version: ", tf.__version__)
-print("TF-Hub version: ", hub.__version__)
-print("Eager mode enabled: ", tf.executing_eagerly())
-print("GPU available: ", tf.config.list_physical_devices('GPU'))
+print('TF Version: ', tf.__version__)
+print('TF-Hub version: ', hub.__version__)
+print('Eager mode enabled: ', tf.executing_eagerly())
+print('GPU available: ', tf.config.list_physical_devices('GPU'))
 
 
 def load_image(image_path, image_size=(256, 256), preserve_aspect_ratio=True):
-  """Loads and preprocesses images."""
+  '''Loads and preprocesses images.'''
 
   # Load and convert to float32 numpy array, add batch dimension, and normalize to range [0, 1]
   img = tf.io.decode_image(
@@ -85,15 +85,15 @@ def post_images():
         filename_stamp = str(timestamp) + str(random_number)
 
         # create paths for image storage
-        content_image_path = f"{filename_stamp}_content_image.jpg" # @param {type:"string"}
-        style_image_path = f"{filename_stamp}_style_image.jpg"  # @param {type:"string"}
+        content_image_path = f'{filename_stamp}_content_image.jpg' # @param {type:'string'}
+        style_image_path = f'{filename_stamp}_style_image.jpg'  # @param {type:'string'}
 
         # save received images for processing
-        request.files["contentImage"].save(content_image_path)    
-        request.files["styleImage"].save(style_image_path)
+        request.files['contentImage'].save(content_image_path)    
+        request.files['styleImage'].save(style_image_path)
 
         # The content image size can be arbitrary.
-        output_image_size = 384  # @param {type:"integer"}
+        output_image_size = 384  # @param {type:'integer'}
         content_img_size = (output_image_size, output_image_size)
         # The style prediction model was trained with image size 256 and it's the 
         # recommended image size for the style image (though, other sizes work as 
@@ -107,33 +107,35 @@ def post_images():
         outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
         stylized_image = outputs[0]
         squeezed_image = tf.squeeze(stylized_image)
-        tf.keras.preprocessing.image.save_img("1.jpg", squeezed_image)
+        tf.keras.preprocessing.image.save_img(f'{filename_stamp}.jpg', squeezed_image)
 
-        return jsonify("message")
-        #return send_file("1.jpg", mimetype='image/jpg')
+        return jsonify(filename_stamp)
+        #return send_file('1.jpg', mimetype='image/jpg')
 
     # handle preflight requests send due to CORS policy
     if request.method == 'OPTIONS':
-        return jsonify("success")
+        return jsonify('success')
 
     if request.method == 'GET':
-        return jsonify("test ping")
+        return jsonify('test ping')
 
 
 # return generated output on request
-@app.route('/api/images/generated_output', methods=['GET'])
+@app.route('/api/images/generated_output/', methods=['GET'])
 #@cross_origin()
 def get_generated_output():
-    return send_file("1.jpg", mimetype='image/jpg')
+    filename = request.args.get('filename')
+    return send_file(f'{filename}.jpg', mimetype='image/jpg')
+
 
 
 # route for req testing
-@app.route("/")
+@app.route('/')
 def get_image():
 
 
-    message = "ping"
-    print("ping")
+    message = 'ping'
+    print('ping')
 
 
     return jsonify(message)
@@ -146,7 +148,7 @@ def get_image():
 ### start the flask app
 if __name__ == '__main__':
     try:
-        app.run(host="0.0.0.0", port="6975", 
+        app.run(host='0.0.0.0', port='6975', 
                 #ssl_context='adhoc', 
                 debug=True,
                 threaded=True, use_reloader=False)
@@ -154,5 +156,5 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         #check for exit
         time.sleep(1)
-        print("exit program")
+        print('exit program')
         sys.exit(0)

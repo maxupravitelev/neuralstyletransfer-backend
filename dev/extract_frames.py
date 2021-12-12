c773b5b275a35d1a75e42508798e2774ec941aaa
Max Upravitelev <max@upravitelev.de>
2021-12-12 14:14:59 +0100

    add basic script for extracting frames from videos

---
+++ dev/extract_frames.py
@@ -0,0 +1,26 @@
+import os
+import cv2
+
+
+filename = ''
+cap = cv2.VideoCapture(filename)
+
+filename_string_length = len(filename)
+
+foldername = filename[:filename_string_length - 4] # delete file extension from string
+
+if not os.path.exists(foldername):
+    os.mkdir(foldername)
+
+count = 0
+while True:
+    ret, image = cap.read()
+
+    if not ret:
+        break
+
+    cv2.imwrite(os.path.join(foldername, f"frame{count}.png"), image)     
+    print(f"{count} frames extracted")
+    count += 1
+
+print(f"{count} images are extracted in {foldername}.")
